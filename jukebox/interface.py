67902c7b8eb80f28e6bea314ca23a2d9011b0a0b
Yves Quemener <Iv@laptop>
2013-09-25 17:16:18 +0900

    Message rendering finished

--- jukebox/interface.py
+++ jukebox/interface.py
@@ -2,12 +2,16 @@
 
 import pygame
 import os
+import sys
 from pyomxplayer import *
 
 from pygame.locals import *
 
 # Init
-fullscreen = False
+fullscreen = True
+
+if len(sys.argv)>1 and sys.argv[1]=="nofs":
+    fullscreen=False
 
 pygame.init()
 if fullscreen:
@@ -17,16 +21,30 @@ else:
 
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
-
+font_big = pygame.font.Font(None, 50)
+font.set_bold(False)
 
 # Handlers
 
-def RenderText(screen, text, xy=[0,0]):
-    for l in text.split("\n"):
-        ts = font.render(l, 1, (255,255,255))
-        screen.blit(text, xy)
-        xy[1] += ts.get_height()
+def RenderText(screen, text, xy=[0,0], style={}):
+    if "font" in style.keys():
+        _font = style["font"]
+    else:
+        _font = font
+    if "bold" in style.keys():
+        _font.set_bold(True)
+    else:
+        _font.set_bold(False)
 
+    for l in text.split("\n"):
+        ts = _font.render(l, 1, (255,255,255))
+        if 'align-center' in style.keys():
+            xy[0]=ts.get_rect(centerx=screen.get_width()/2)[0]
+        else:
+            xy[0] = 0
+        screen.blit(ts, xy)
+        xy[1] += ts.get_height()
+    return xy
 def OnClick(event):
     context['header_text_color'][0] = 255-context['header_text_color'][0] 
     try:
@@ -41,12 +59,24 @@ context = {
         'header_text_color':[255,255,255]
         }
 
-message = """Tokyo HackerSpace Entertainment System
-Don't try this at home: this is what the hackerspace is for!"""
+title = """Tokyo HackerSpace Entertainment System
+Don't try this at home, this is what the hackerspace is for!"""
+ 
+message="""My IP address is 192.168.1.20
+I am a MPD daemon. Yeah baby }:) 
+You can access it through the default port (6600) with clients like gmpc.
+ 
+You can ssh me as user 'pi' with the password 'raspberry'.
+To make me play a video, ssh me and use the command line "omxplayer <mp4 file>". 
+There are some files in Videos/ you are welcomed to add more.
+ 
+Touch this screen if you need some badger support."""
 running = True
 while running:
     clock.tick(60)
-    RenderText(screen,message)
+    pos = RenderText(screen,title, [0,0], {'align-center':"",'bold':"",'font':font_big})
+    RenderText(screen,message, [pos[0],pos[1]+200])
+
 
     for event in pygame.event.get():
         if event.type == QUIT or event.type == KEYDOWN:
